c50df78ddbd1fda185fc752c1d9feeedb7878872
Eduardo Lopes Arrais de Oliveira <eduardoarrais2013@gmail.com>
2025-03-17 08:40:58 -0300

    Delete orientacao-a-objetos/exercicios directory

--- orientacao-a-objetos/exercicios/biblioteca.py
+++
@@ -1,5 +0,0 @@
-from modelos.livro import Livro
-
-livro1 = Livro('Livro 1', 'Livro 2', 20)
-livro1.emprestar()
-print(f'{livro1._disponivel}')
--- orientacao-a-objetos/exercicios/concessionaria/carro.py
+++
@@ -1,9 +0,0 @@
-from veiculo import Veiculo
-
-class Carro(Veiculo):
-    def __init__(self, marca, modelo, cor):
-        super().__init__(marca, modelo)
-        self._cor = cor
-    
-    def ligar(self):
-        print(f'O carro {self._modelo} está ligado')
